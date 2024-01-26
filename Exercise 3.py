bd1a4974e4eda8ba1b818cd721227fff54d43211
Jazper26 <jazperhaapin89Gmail.com>
2024-01-26 18:47:27 +0800

    Using range to itterate even index characters and print

--- Exercise 3.py
+++ Exercise 3.py
@@ -2,4 +2,7 @@ def display_even_index_characters(input_str):
     # Use slicing with a step of 2 to get characters at even indices
     even_index_characters = input_str[::2]
 
-    
+    # Iterate through each character and print it
+    for char in even_index_characters:
+        print(char)
+
